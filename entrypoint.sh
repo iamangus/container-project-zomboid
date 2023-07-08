9ccc6217a3ce3cb9fe3b7caa2b8ed7d0e9e2c3c8
iamangus <turnbull.angus@gmail.com>
2023-07-08 18:51:53 -0500

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 mkdir ~/pzserver
-steamcmd +force_install_dir ~/pzserver +login anonymous +app_update 376030 +quit
+steamcmd +force_install_dir ~/pzserver +login anonymous +app_update 380870 +quit
 # Fix to a bug in start-server.sh that causes to no preload a library:
 # https://github.com/Danixu/project-zomboid-server-docker/blob/main/scripts/entry.sh
 # ERROR: ld.so: object 'libjsig.so' from LD_PRELOAD cannot be preloaded (cannot open shared object file): ignored.
