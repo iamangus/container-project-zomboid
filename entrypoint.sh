e31b7d0cb06ce3782e5f162be569b71931ee0d20
iamangus <turnbull.angus@gmail.com>
2023-07-08 19:22:01 -0500

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,4 @@ steamcmd +force_install_dir ~/pzserver +login anonymous +app_update 380870 +quit
 # https://github.com/Danixu/project-zomboid-server-docker/blob/main/scripts/entry.sh
 # ERROR: ld.so: object 'libjsig.so' from LD_PRELOAD cannot be preloaded (cannot open shared object file): ignored.
 export LD_LIBRARY_PATH="~/pzserver/jre64/lib:${LD_LIBRARY_PATH}"
-ls ~
-ls ~/pzserver
-bash ~/pzserver/start-server.sh
+bash ~/pzserver/start-server.sh -adminpassword "temp"
